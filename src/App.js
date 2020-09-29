7df127c1c4d680716747f9360124a0c3dfcf0066
ljsnell <ljsnell@users.noreply.github.com>
2020-09-28 23:34:12 -0500

    Jump button (#2)
    * Mostly working.
    
    * Added show full question button.
    
    * Added start function to page load.
    
    * Improved error trapping.
    
    * Populated default value.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       data: "",
       question: "",
       answer: "🤔",
-      showAnswer: false,
       qtype: 1,
       selectedChapters: [1,2,3,4,5],
       questionTypes: [{value: 1, label: "General"},
@@ -22,11 +21,20 @@ class App extends Component {
                       {value: 6, label: "Multiple Part"},
                       {value: 7, label: "FTV"},
                       {value: 8, label: "Reference"},
-                      {value: 9, label: "Situation"}],      
+                      {value: 9, label: "Situation"}],
+      question_array: [],
+      q_text_to_display: "",
+      i: 0
     };
   }
   
-  getQuestion() {    
+  componentDidMount() {
+    setInterval(() => this.startQuiz(),1000)
+  }
+
+  getQuestion() {
+    this.setState({q_text_to_display: ""})
+    this.setState({i: 0})
     // fetch('http://localhost:5000/filtered?qtype=' + this.state.qtype + '&books=1&chapters=' +
     //  this.state.selectedChapters)
     fetch('https://bq-questions-api.uc.r.appspot.com//filtered?qtype=' + this.state.qtype + '&books=1&chapters=' +
@@ -38,6 +46,7 @@ class App extends Component {
           this.setState({question: (data[9] + data[15])})
           this.setState({data: data})
           this.setState({answer: "🧐"})
+          this.setState({question_array: this.state.question.split(" ")})
         } else {
           this.setState({question: 'No question found for that criteria, FeelsBadMan'})
           this.setState({answer: "😭"})
@@ -52,6 +61,32 @@ class App extends Component {
     }    
   }
 
+  jump() {
+    console.log('question_array')
+    console.log(this.state.question_array)
+    this.setState({i: this.state.question_array.length})
+  }
+
+  showFullQuestion() {
+    this.setState({q_text_to_display: this.state.question})
+    this.setState({i: this.state.question_array.length})
+  }
+    
+  startQuiz() {
+    var {
+      question,
+      q_text_to_display,
+      i
+    } = this.state
+
+    this.question_array = question.split(" ")
+    if (i < this.question_array.length) {
+        q_text_to_display = q_text_to_display.concat(this.question_array[i]).concat(' ')
+        i++
+        this.setState({ q_text_to_display: q_text_to_display, i: i })
+      }
+  }
+
   handleQTypeChange(e){
     this.setState({qtype:e.value})
    }
@@ -64,16 +99,18 @@ class App extends Component {
     return (
       <React.Fragment>
         <div>
-          <h3>Question: {this.state.question}</h3>
+          <h3>Question: {this.state.q_text_to_display}</h3>
           <h3>Answer: {this.state.answer}</h3>
         </div>
         <div>
         <Button onClick={()=>this.getQuestion()} variant="secondary">Next Question</Button>{' '}
+        <Button onClick={()=>this.showFullQuestion()} variant="secondary">Full Question</Button>{' '}
         <Button onClick={()=>this.showAnswer()} variant="secondary">Show Answer</Button>{' '}
+        <Button onClick={()=>this.jump()} variant="secondary">Jump</Button>{' '}        
         </div>
         <div>
           Question Type:
-          <Select options={this.state.questionTypes} onChange={this.handleQTypeChange.bind(this)}/>
+          <Select defaultValue={this.state.questionTypes[0]} options={this.state.questionTypes} onChange={this.handleQTypeChange.bind(this)}/>
         </div>
         <div>
           Selected Chapters (e.g. 1,2,3 for chapters 1-3)
